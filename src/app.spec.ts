003caecbfd0ae797438115de96ca29df20135773
Sudarsan Balaji <hello@artful.dev>
2024-10-06 08:44:20 +0100

    Refactor out response mechanism

--- src/app.spec.ts
+++ src/app.spec.ts
@@ -3,34 +3,32 @@ import { app } from "./app";
 import { reduce, Subject } from "rxjs";
 
 describe("app", () => {
-  test("should handshake", () => {
+  const response = (command: string) => {
     const line$ = new Subject<string>();
     const { stdout } = app({ stdin: { line$ } });
-    stdout
-      .pipe(reduce((lines, line) => lines.concat(line), [] as string[]))
-      .subscribe((lines) => {
-        expect(lines).toEqual(["st3p version 2 ok"]);
-      });
-    line$.next("st3p version 2");
+    const lines = stdout.pipe(
+      reduce((lines, line) => lines.concat(line), [] as string[])
+    );
+    line$.next(command);
+    return lines;
+  };
+
+  test("should handshake", () => {
+    response("st3p version 2").subscribe((lines) => {
+      expect(lines).toEqual(["st3p version 2 ok"]);
+    });
   });
 
   test("should identify", () => {
-    const line$ = new Subject<string>();
-    const { stdout } = app({ stdin: { line$ } });
-    stdout
-      .pipe(reduce((lines, line) => lines.concat(line), [] as string[]))
-      .subscribe((lines) => {
-        const identifications = lines.map(
-          (line) => line.slice(9).split(" ")[0]
-        );
-        expect(identifications).toEqual([
-          "name",
-          "author",
-          "version",
-          "url",
-          "ok",
-        ]);
-      });
-    line$.next("identify");
+    response("identify").subscribe((lines) => {
+      const identifications = lines.map((line) => line.slice(9).split(" ")[0]);
+      expect(identifications).toEqual([
+        "name",
+        "author",
+        "version",
+        "url",
+        "ok",
+      ]);
+    });
   });
 });
