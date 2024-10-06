ffed3a48d4c9600ddac88c8398b2f45f424dd97e
Sudarsan Balaji <hello@artful.dev>
2024-10-06 23:45:05 +0100

    Return moves faster

--- src/core/move.ts
+++ src/core/move.ts
@@ -3,20 +3,8 @@ import type { Board } from "../t3en";
 export type Move = BigInt;
 
 export const moves = ({ playable, size }: Board): Move[] => {
-  const length = size ** 2;
-  return playable
-    .toString(2)
-    .padStart(size * size, "0")
-    .split("")
-    .map((cell, i) => [cell, i] as const)
-    .filter(([cell]) => cell === "1")
-    .map(([, index]) =>
-      BigInt(
-        "0b" +
-          Array.from({ length })
-            .fill(0)
-            .map((_, i) => (i === index ? 1 : 0))
-            .join("")
-      )
-    );
+  return Array.from<bigint>({ length: size ** 2 })
+    .fill(BigInt(1))
+    .map((x, i) => x << BigInt(i))
+    .filter((move) => (move & playable) === move);
 };
