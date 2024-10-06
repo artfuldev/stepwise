faaa779a5a3479e180ba920069cbc95ca6451a46
Sudarsan Balaji <hello@artful.dev>
2024-10-06 10:31:24 +0100

    Rework best move to work on a bitboard

--- src/core/best.ts
+++ src/core/best.ts
@@ -1,19 +1,22 @@
-import { type Board, Cell } from "../t3en";
+import { type Board } from "../t3en";
 
 const random = <A>(as: A[]): A => {
   const index = Math.floor(Math.random() * as.length);
   return as[index];
 };
 
-export const best = async (
-  board: Board
-): Promise<readonly [number, number]> => {
-  const indices = board
-    .flatMap((row, i) =>
-      row.map((cell, j) => [i, j, cell === Cell.Playable] as const)
-    )
-    .filter(([, , playable]) => playable);
+export const best = async ({
+  size,
+  playable,
+}: Board): Promise<readonly [number, number]> => {
+  const indices = playable
+    .toString(2)
+    .padStart(size * size, "0")
+    .split("")
+    .map((cell, i) => [cell, i] as const)
+    .filter(([cell]) => cell === "1")
+    .map(([, index]) => index);
   if (indices.length === 0) throw new Error("no playable cells");
-  const [x, y] = random(indices);
-  return [x, y];
+  const index = random(indices);
+  return [Math.floor(index / size), index % size];
 };
