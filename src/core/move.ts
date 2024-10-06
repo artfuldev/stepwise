bd940b9c39a81b760198e928bfa4bb0b8dd4329d
Sudarsan Balaji <hello@artful.dev>
2024-10-06 23:53:54 +0100

    Simplify

--- src/core/move.ts
+++ src/core/move.ts
@@ -1,10 +1,8 @@
 import type { Board } from "../t3en";
 
-export type Move = BigInt;
+export type Move = bigint;
 
-export const moves = ({ playable, size }: Board): Move[] => {
-  return Array.from<bigint>({ length: size ** 2 })
-    .fill(BigInt(1))
-    .map((x, i) => x << BigInt(i))
+export const moves = ({ playable, size }: Board): Move[] =>
+  Array.from({ length: size ** 2 })
+    .map((_, i) => BigInt(1) << BigInt(i))
     .filter((move) => (move & playable) === move);
-};
