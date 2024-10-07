891339a34fec154a1e43f19e361cc16614ec5c4d
Sudarsan Balaji <hello@artful.dev>
2024-10-07 20:30:29 +0100

    Remove unnecessary code

--- src/st3p/move.ts
+++ src/st3p/move.ts
@@ -58,17 +58,6 @@ export const parse = pipe(
   P.map(([, , state, time]) => Move(state, time))
 );
 
-const column = (index: number) => {
-  let result = "";
-  do {
-    let remainder = index % 26;
-    result = String.fromCharCode(97 + remainder) + result;
-    index = Math.floor(index / 26) - 1;
-  } while (index >= 0);
-
-  return result;
-};
-
 export const move = ([, [board]]: Move): Sinks => {
   const best$ = best(board).pipe(
     map(position(board)),
