3935cecf27fa3b01dcdeb06d1eb04c26377ce2ea
Sudarsan Balaji <hello@artful.dev>
2024-10-09 22:41:36 +0100

    Fix tests

--- src/app.spec.ts
+++ src/app.spec.ts
@@ -1,74 +1,83 @@
 import { describe, expect, test } from "bun:test";
 import { app } from "./app";
-import { reduce, Subject } from "rxjs";
+import { of, reduce, shareReplay, Subject, take, tap, timeout } from "rxjs";
 
 describe("app", () => {
   const response = (command: string) => {
     const line$ = new Subject<string>();
-    const { stdout } = app({ stdin: { line$ } });
-    const lines = stdout.pipe(
-      reduce((lines, line) => lines.concat(line), [] as string[])
-    );
-    line$.next(command);
-    return lines;
+    const sinks = app({ stdin: { line$ } });
+    queueMicrotask(() => line$.next(command));
+    return sinks;
   };
 
-  test("should handshake", () => {
-    response("st3p version 2").subscribe(([line]) => {
+  test("should handshake", (done) => {
+    response("st3p version 2").stdout.subscribe((line) => {
       expect(line).toEqual("st3p version 2 ok");
+      done();
     });
   });
 
-  test("should identify", () => {
-    response("identify").subscribe((lines) => {
-      const identifications = lines.map((line) => line.slice(9).split(" ")[0]);
-      expect(identifications).toEqual([
-        "name",
-        "author",
-        "version",
-        "url",
-        "ok",
-      ]);
-    });
+  test("should identify", (done) => {
+    response("identify")
+      .stdout.pipe(
+        take(5),
+        reduce((lines, line) => lines.concat(line), [] as string[])
+      )
+      .subscribe((lines) => {
+        const identifications = lines.map(
+          (line) => line.slice(9).split(" ")[0]
+        );
+        expect(identifications).toEqual([
+          "name",
+          "author",
+          "version",
+          "url",
+          "ok",
+        ]);
+        done();
+      });
   });
 
-  test("should return best move", () => {
-    response("move 3_/3_/3_ x").subscribe(([line]) => {
+  test("should return best move", (done) => {
+    response("move 3_/3_/3_ x").stdout.subscribe((line) => {
       expect(line).toStartWith("best ");
+      done();
     });
   });
 
-  test("should return valid move", () => {
+  test("should return valid move", (done) => {
     const columns = "abc".split("");
     const rows = [1, 2, 3];
-    const moves = columns.map((c) => rows.map((r) => [c, r].join("")));
-    response("move 3_/3_/3_ x").subscribe(([line]) => {
+    const moves = columns.flatMap((c) => rows.map((r) => [c, r].join("")));
+    response("move 3_/3_/3_ x").stdout.subscribe((line) => {
       const move = line.slice(5);
       expect(move).toBeOneOf(moves);
+      done();
     });
   });
 
-  test("should error out on unknown commands", () => {
-    const line$ = new Subject<string>();
-    const { stderr } = app({ stdin: { line$ } });
-    stderr
-      .pipe(reduce((lines, line) => lines.concat(line), [] as string[]))
-      .subscribe(([err]) => expect(err).toBeTypeOf("string"));
-    line$.next("unknown command");
+  test("should error out on unknown commands", (done) => {
+    response("unknown command").stderr.subscribe((err) => {
+      expect(err).toBeTypeOf("string");
+      done();
+    });
   });
 
-  test("should not have outputs on unknown commands", () => {
-    response("unknown command").subscribe((lines) =>
-      expect(lines).toHaveLength(0)
-    );
+  test("should not have outputs on unknown commands", (done) => {
+    response("unknown command")
+      .stdout.pipe(timeout({ first: 100 }))
+      .subscribe({
+        error: (error) => {
+          expect(error.message).toBe("Timeout has occurred");
+          done();
+        },
+      });
   });
 
-  test("should quit", () => {
-    const line$ = new Subject<string>();
-    const { exit } = app({ stdin: { line$ } });
-    exit
-      .pipe(reduce((lines, line) => lines.concat(line), [] as number[]))
-      .subscribe(([exit]) => expect(exit).toEqual(0));
-    line$.next("quit");
+  test("should quit", (done) => {
+    response("quit").exit.subscribe((exit) => {
+      expect(exit).toEqual(0);
+      done();
+    });
   });
 });
