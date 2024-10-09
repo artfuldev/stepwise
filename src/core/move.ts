e17a680d826997f4d07985294dd903add9d41fb1
Sudarsan Balaji <hello@artful.dev>
2024-10-09 17:52:29 +0100

    Allow playing a move

--- src/core/move.ts
+++ src/core/move.ts
@@ -1,4 +1,4 @@
-import type { Board } from "../t3en";
+import type { Board, Side } from "../t3en";
 
 export type Move = bigint;
 
@@ -11,4 +11,12 @@ export const moves = ({ playable, size }: Board): Move[] => {
     move = move << 1n;
   }
   return moves;
+};
+
+export const play = (board: Board, side: Side, move: Move): Board => {
+  const mask = (1n << BigInt(board.size * board.size)) - 1n;
+  const playable = board.playable & mask & (~(move & mask) & mask);
+  const next = board[side] | move;
+  const next_board ={ ...board, playable, [side]: next };
+  return next_board;
 };
