f63fbfff2071b6626d3354fdfb2491da107d57eb
Sudarsan Balaji <hello@artful.dev>
2024-10-09 22:08:30 +0100

    Use observables over promises

--- src/core/best.spec.ts
+++ src/core/best.spec.ts
@@ -1,28 +1,31 @@
 import { describe, expect, test } from "bun:test";
 import { best } from "./best";
 import { Side, type Board } from "../t3en";
-import { firstValueFrom } from "rxjs";
 
 describe("best", () => {
-  test("should return winning move for o", async () => {
+  test("should return winning move for o", (done) => {
     const board: Board = {
       size: 3,
       playable: BigInt("0b011100100"),
       x: BigInt("0b100011000"),
       o: BigInt("0b000000011"),
     };
-    const move = await firstValueFrom(best(board, Side.O, 3));
-    expect(move).toBe(BigInt("0b000000100"));
+    best(board, Side.O, 3).subscribe((move) => {
+      expect(move).toBe(BigInt("0b000000100"));
+      done();
+    });
   });
 
-  test("should return winning move for x", async () => {
+  test("should return winning move for x", (done) => {
     const board: Board = {
       size: 3,
       playable: BigInt("0b011100100"),
       x: BigInt("0b100011000"),
       o: BigInt("0b000000011"),
     };
-    const move = await firstValueFrom(best(board, Side.X, 3));
-    expect(move).toBe(BigInt("0b000100000"));
+    best(board, Side.X, 3).subscribe(move => {
+      expect(move).toBe(BigInt("0b000100000"));
+      done();
+    })
   });
 });
