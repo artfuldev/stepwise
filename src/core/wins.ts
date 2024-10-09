e01680b456829b693da0816b5627aa363ec184b9
Sudarsan Balaji <hello@artful.dev>
2024-10-09 10:17:01 +0100

    Improve performance
    by reducing iterations

--- src/core/wins.ts
+++ src/core/wins.ts
@@ -2,49 +2,37 @@ export const wins = (size: number, winLength = size): bigint[] => {
   winLength = Math.min(size, winLength);
   const threshold = size - winLength + 1;
   const wins: bigint[] = [];
-
-  // Horizontal
+  
   let thresh = threshold === 1 ? BigInt(size) : BigInt(threshold);
-  let win = (1n << BigInt(winLength)) - 1n;
+  let horizontal = (1n << BigInt(winLength)) - 1n;
+  let vertical = 0n;
+  for (let k = 0; k < winLength; k++) {
+    vertical |= 1n << BigInt(k * size);
+  }
   let shift = 1n;
   for (let i = 0; i < size * threshold; i++) {
-    wins.push(win);
+    wins.push(horizontal);
+    wins.push(vertical);
     shift = (i + 1) % threshold === 0 ? thresh : 1n;
-    win = win << shift;
+    horizontal = horizontal << shift;
+    vertical = vertical << 1n;
   }
-
-  // Vertical
-  win = 0n;
+  let diagonal = 0n;
   for (let k = 0; k < winLength; k++) {
-    win |= 1n << BigInt(k * size);
+    diagonal |= 1n << BigInt(k * size + k);
   }
-  shift = 1n;
-  for (let i = 0; i < size * threshold; i++) {
-    wins.push(win);
-    win = win << shift;
+  let anti = 0n;
+  for (let k = 0; k < winLength; k++) {
+    anti |= 1n << BigInt(k * size + (winLength - 1 - k));
   }
-
-  // Diagonal
-  win = 0n;
   thresh = BigInt(threshold);
-  for (let k = 0; k < winLength; k++) {
-    win |= 1n << BigInt(k * size + k);
-  }
   for (let i = 0; i < threshold * threshold; i++) {
-    wins.push(win);
+    wins.push(diagonal);
+    wins.push(anti);
     shift = (i + 1) % threshold === 0 ? thresh : 1n;
-    win = win << shift;
+    diagonal = diagonal << shift;
+    anti = anti << shift;
   }
 
-  // Anti Diagonal
-  win = 0n;
-  for (let k = 0; k < winLength; k++) {
-    win |= 1n << BigInt(k * size + (winLength - 1 - k));
-  }
-  for (let i = 0; i < threshold * threshold; i++) {
-    wins.push(win);
-    shift = (i + 1) % threshold === 0 ? thresh : 1n;
-    win = win << shift;
-  }
   return wins;
 };
