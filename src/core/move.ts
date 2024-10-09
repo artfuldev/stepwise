632187f0eadf203f176861ecc649ad94a16dc9db
Sudarsan Balaji <hello@artful.dev>
2024-10-09 14:39:10 +0100

    Improve performance

--- src/core/move.ts
+++ src/core/move.ts
@@ -2,7 +2,13 @@ import type { Board } from "../t3en";
 
 export type Move = bigint;
 
-export const moves = ({ playable, size }: Board): Move[] =>
-  Array.from({ length: size ** 2 })
-    .map((_, i) => BigInt(1) << BigInt(i))
-    .filter((move) => (move & playable) === move);
+export const moves = ({ playable, size }: Board): Move[] => {
+  const moves: Move[] = [];
+  const max = size ** 2;
+  let move = 1n;
+  for (let i = 0; i < max; i++) {
+    if ((move & playable) === move) moves.push(move);
+    move << 1n;
+  }
+  return moves;
+};
