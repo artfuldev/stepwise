72a2556a12b065caf5b447ba6df839a84edc7717
Sudarsan Balaji <hello@artful.dev>
2024-10-10 00:08:46 +0100

    Cache ones

--- src/core/evaluate.ts
+++ src/core/evaluate.ts
@@ -2,12 +2,17 @@ import type { Board, Side } from "../t3en";
 import { other } from "../t3en/side";
 import { wins } from "./wins";
 
-const ones = (bigint: bigint) => {
+let ones_cache = new Map<bigint, number>();
+
+const ones = (bigint: bigint): number => {
+  const cached = ones_cache.get(bigint);
+  if (cached != null) return cached;
   let count = 0;
   while (bigint !== 0n) {
     bigint = bigint & (bigint - 1n);
     count++;
   }
+  ones_cache.set(bigint, count);
   return count;
 };
 
