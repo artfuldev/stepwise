96b87b57898c04e33846da4b08527cda2941c4a8
Sudarsan Balaji <hello@artful.dev>
2024-10-10 11:08:20 +0100

    Simplify

--- src/core/best.spec.ts
+++ src/core/best.spec.ts
@@ -1,29 +1,21 @@
 import { describe, expect, test } from "bun:test";
 import { best } from "./best";
 import { Side, type Board } from "../t3en";
+import { parse } from "../t3en/board";
+import type { ParseSuccess } from "../parser";
 
 describe("best", () => {
+  const board = (value: string) => (parse(value) as ParseSuccess<Board>).parsed;
+
   test("should return winning move for o", (done) => {
-    const board: Board = {
-      size: 3,
-      playable: BigInt("0b011100100"),
-      x: BigInt("0b100011000"),
-      o: BigInt("0b000000011"),
-    };
-    best(board, Side.O, 3).subscribe((move) => {
+    best(board("x2_/_2x/_2o"), Side.O).subscribe((move) => {
       expect(move).toBe(BigInt("0b000000100"));
       done();
     });
   });
 
   test("should return winning move for x", (done) => {
-    const board: Board = {
-      size: 3,
-      playable: BigInt("0b011100100"),
-      x: BigInt("0b100011000"),
-      o: BigInt("0b000000011"),
-    };
-    best(board, Side.X, 3).subscribe((move) => {
+    best(board("x2_/_2x/_2o"), Side.X).subscribe((move) => {
       expect(move).toBe(BigInt("0b000100000"));
       done();
     });
