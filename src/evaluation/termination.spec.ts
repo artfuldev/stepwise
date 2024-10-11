86ebcdca0194e93656b74fd78b85d287a642b490
Sudarsan Balaji <hello@artful.dev>
2024-10-11 14:00:24 +0100

    Add spec for termination

---
+++ src/evaluation/termination.spec.ts
@@ -0,0 +1,31 @@
+import { describe, expect, test } from "bun:test";
+import { type Board } from "../t3en";
+import { parse } from "../t3en/board";
+import type { ParseSuccess } from "../parser";
+import type { Game } from "../core/game";
+import { Termination, termination } from "./termination";
+
+describe("termination", () => {
+  const board = (value: string) => (parse(value) as ParseSuccess<Board>).parsed;
+  const game = (board: Board, winLength = board.size): Game => ({
+    ...board,
+    xToPlay: true,
+    winLength,
+  });
+
+  test("should return x wins", () => {
+    expect(termination(game(board("x_x/_xo/x_o")))).toBe(Termination.XWon);
+  });
+
+  test("should return o wins", () => {
+    expect(termination(game(board("x_o/_xo/x_o")))).toBe(Termination.OWon);
+  });
+
+  test("should return draws", () => {
+    expect(termination(game(board("xox/xox/oxo")))).toBe(Termination.Drawn);
+  });
+
+  test("should return lack of termination", () => {
+    expect(termination(game(board("xox/xox/o_o")))).toBe(Termination.None);
+  });
+});
