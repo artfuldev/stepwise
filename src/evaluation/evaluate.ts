e82304e890c978a9c0cc0d8b3b3ad29106872f21
Sudarsan Balaji <hello@artful.dev>
2024-10-11 14:01:38 +0100

    Fix side to play as maximizing player

--- src/evaluation/evaluate.ts
+++ src/evaluation/evaluate.ts
@@ -1,4 +1,4 @@
 import type { Game } from "../core/game";
 import { minimax } from "./minimax";
 
-export const evaluate = (game: Game) => minimax(game, 2, !game.xToPlay);
+export const evaluate = (game: Game) => minimax(game, 2, game.xToPlay);
