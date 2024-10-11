73468c3d2f650089eb44f0cbff5253c06605df3e
Sudarsan Balaji <hello@artful.dev>
2024-10-11 14:01:26 +0100

    Use integers and score earlier wins better

--- src/evaluation/heuristic.ts
+++ src/evaluation/heuristic.ts
@@ -8,8 +8,10 @@ import { Termination, termination } from "./termination";
 
 export const heuristic = memoize((game: Game): number => {
   let result = termination(game);
-  if (result === Termination.XWon) return Number.POSITIVE_INFINITY;
-  if (result === Termination.OWon) return Number.NEGATIVE_INFINITY;
+  if (result === Termination.XWon)
+    return Number.MAX_SAFE_INTEGER - game.size ** 2 + ones(game.playable);
+  if (result === Termination.OWon)
+    return Number.MIN_SAFE_INTEGER + game.size ** 2 - ones(game.playable);
   if (result === Termination.Drawn) return 0;
   let score = 0;
   for (const win of wins(game.size, game.winLength)) {
@@ -19,9 +21,11 @@ export const heuristic = memoize((game: Game): number => {
   for (const [playable, played] of assurances(game.size, game.winLength)) {
     if ((game.playable & playable) !== playable) continue;
     const x = played & game.x;
-    if (x === played) return Number.MAX_SAFE_INTEGER;
+    if (x === played)
+      return Number.MAX_SAFE_INTEGER / 2 - game.size ** 2 + ones(game.playable);
     const o = played & game.o;
-    if (o === played) return Number.MIN_SAFE_INTEGER;
+    if (o === played)
+      return Number.MIN_SAFE_INTEGER / 2 + game.size ** 2 - ones(game.playable);
   }
   return score;
 }, key);
