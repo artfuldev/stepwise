6ef3d1309b1552adb998595ebe7c98228695bcaa
Sudarsan Balaji <hello@artful.dev>
2024-10-15 23:57:06 +0100

    Shuffle moves before evaluation

--- src/evaluation/alpha-beta-pvs.ts
+++ src/evaluation/alpha-beta-pvs.ts
@@ -8,12 +8,13 @@ import { position } from '../core/position';
 import { string } from '../t3en/board';
 import { heuristic } from './heuristic';
 import { ones } from './ones';
+import { shuffle } from './shuffle';
 import { terminal } from './terminal';
 
 const log = debug('stepwise').extend('pvs');
 
 const f = AlphaBetaPrincipleVariationSearch.create(terminal)((game) =>
-  moves(game),
+  shuffle(moves(game)),
 )(make)(unmake)(heuristic);
 
 const TOLERANCE = Duration.fromMillis(100);
