36bed43d823f994277acb1038f0b40e37cd10d71
Sudarsan Balaji <hello@artful.dev>
2024-10-15 14:08:59 +0100

    Return game after make and unmake

--- src/core/game.ts
+++ src/core/game.ts
@@ -7,18 +7,21 @@ export type Game = Board & {
 };
 
 export const make =
-  (game: Game, move: Move) => {
+  (game: Game, move: Move): Game => {
     const mask = (1n << BigInt(game.size * game.size)) - 1n;
     game.playable &= mask ^ move;
     const side = game.xToPlay ? Side.X : Side.O;
     game.xToPlay = !game.xToPlay;
     game[side] |= move;
+    return game;
   };
 
 export const unmake =
-  (game: Game, move: Move) => {
+  (game: Game, move: Move): Game => {
     const mask = (1n << BigInt(game.size * game.size)) - 1n;
     game.playable |= move;
     const side = game.xToPlay ? Side.O : Side.X;
     game[side] &= mask ^ move;
+    game.xToPlay = !game.xToPlay;
+    return game;
   };
