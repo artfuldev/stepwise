e371642c10a323774dcc88bca37f001cec36d0fc
Sudarsan Balaji <hello@artful.dev>
2024-10-15 11:20:39 +0100

    Add make and unmake

--- src/core/game.ts
+++ src/core/game.ts
@@ -1,6 +1,24 @@
-import type { Board } from "../t3en";
+import { Side, type Board } from "../t3en";
+import type { Move } from "./move";
 
 export type Game = Board & {
   xToPlay: boolean;
   winLength: number;
 };
+
+export const make =
+  (game: Game, move: Move) => {
+    const mask = (1n << BigInt(game.size * game.size)) - 1n;
+    game.playable &= mask ^ move;
+    const side = game.xToPlay ? Side.X : Side.O;
+    game.xToPlay = !game.xToPlay;
+    game[side] |= move;
+  };
+
+export const unmake =
+  (game: Game, move: Move) => {
+    const mask = (1n << BigInt(game.size * game.size)) - 1n;
+    game.playable |= move;
+    const side = game.xToPlay ? Side.O : Side.X;
+    game[side] &= mask ^ move;
+  };
