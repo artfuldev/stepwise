d0903ecbebe627de216d6d19dc334b6eb4eb3bf1
Sudarsan Balaji <hello@artful.dev>
2024-10-17 13:25:00 +0100

    Fix specifictions

--- src/bits/ones.spec.ts
+++ src/bits/ones.spec.ts
@@ -1,12 +1,12 @@
 import { describe, expect, test } from 'bun:test';
 import { ones } from './ones';
 
-describe('trailing-zeroes', () => {
+describe('ones', () => {
   test('should return 0 for 0', () => {
     expect(ones(0n)).toBe(0);
   });
 
   test('should return count of ones', () => {
-    expect(ones(1001000n)).toBe(2);
+    expect(ones(0b1001000n)).toBe(2);
   });
 });
